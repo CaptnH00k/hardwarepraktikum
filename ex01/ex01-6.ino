5f8acba3ef3b43986f480ccf89750832063ae387
Ativelox <cormag97@web.de>
2017-05-04 22:38:33 +0200

    Added comments and documentation, aswell as better code structure for the edited code.
    Signed-off-by: Ativelox <cormag97@web.de>

--- ex01/ex01-6.ino
+++ ex01/ex01-6.ino
@@ -10,76 +10,153 @@
  DB7 : Pin 9
 */
 
-// include LCD functions:
+// include the library used to control the LCD attached to the arduino board. 
 #include <LiquidCrystal.h>
 
-// define the LCD screen
-LiquidCrystal lcd(3, 4, 6, 7, 8, 9);
+/*
+ * A constang holding the reference to a LiquidCrystal object (the lcd on the board). 
+ * The constructer used is specified as LiquidCrystal(rs, enable, d4, d5, d6, d7).
+ * 
+ * See the available documentation here:
+ * https://www.arduino.cc/en/Reference/LiquidCrystalConstructor
+ */
+const LiquidCrystal lcd(3, 4, 6, 7, 8, 9);
 
-// global variable, stores the result from analog pin
-int analogValue;
+/**
+ * The time in milliseconds that should be waited after "reprinting" onto the LCD used.
+ * Writing to the LCD can cause flickering otherwise, if written onto the LCD too often.
+ */
+const int FLICKER_PREVENTION_TIME = 100;
 
-void setup()
-{
-  // LCD has 4 lines with 20 chars
+/**
+ * The value needed to identify the first button by the value of the analog pin.
+ */
+const int FIRST_BUTTON_THRESHOLD = 121;
+
+/**
+ * The value needed to identify the second button by the value of the analog pin.
+ */
+const int SECOND_BUTTON_THRESHOLD = 357;
+
+/**
+ * The value needed to identify the third button by the value of the analog pin.
+ */
+const int THIRD_BUTTON_THRESHOLD = 575;
+
+/**
+ * The value needed to identify the fourth button by the value of the analog pin.
+ */
+const int FOURTH_BUTTON_THRESHOLD = 753;
+
+/**
+ * The value needed to identify the fifth button by the value of the analog pin.
+ */
+const int FIFTH_BUTTON_THRESHOLD = 927;
+
+/*
+ * This method gets called once, before the update cycle starts ticking.
+ */
+void setup() {
+  // initializes the interface to the LCD with 4 rows and 20 chars per row
   lcd.begin(20, 4);
+  
 }
 
+/*
+ * The main-loop of this program. Gets repeatedly called without delay.
+ * 
+ * @see ex01-6#lcdLogic()
+ */
+void loop() {
+  // the value of the analog pin A0.
+  int analogValue = analogRead(A0);
 
-void loop()
-{
-  // Because values are not static, we always use the middle between
-  analogValue = analogRead(A0);
-  lcd.clear(); // Clear display to not overwrite stupid things
-  lcd.setCursor(0,0); // Set cursor to first line
-  lcd.print("Analog 0: " + String(analogValue)); // print out the analog value
-  lcd.setCursor(0,1); // set cursor to second line
-  lcd.print("button: " + whichButtonIsPressedAsString()); // print out which button is pressed
-   // wait for 100 ms (reduces display flickering)
-  delay(100);
+  lcdLogic(analogValue);
 }
 
-// Returns which button is pressed as a string. "S1-5" => Button 1-5, "-"=> no button is pressed
-String whichButtonIsPressedAsString() {
-  int buttonPressed = whichButtonIsPressed();
-  if(buttonPressed == 0){
-    return "-";
-  } else {
-    return "S"+String(buttonPressed);
-  }
-}
-
-// Returns which button is pressed. 1-5 => Button 1-5, 0=> no button is pressed
-int whichButtonIsPressed()
-{
-  // Because values are not static, we always use the middle between
-  // the value for this button and the next higher.
-  // Eg: Button1: 0, Button2: 241 ((241 + 0) / 2) + 0 = 121 
-  if(analogValue < 121) {
-    return 1;
-  }else if(analogValue < 357) {
-    return 2;
-  }else if(analogValue < 575) {
-    return 3;
-  }else if(analogValue < 753) {
-    return 4;
-  }else if(analogValue < 927) {
-    return 5;
-  }else {
-    // no button pressed
-    return 0;
-  }
-}
-
-
-/* Usefull LCD functions:
-set the current write position of the lcd to specific line and row:
-  lcd.setCursor(row, line);
-
-write onto lcd, starting at current position:
-  lcd.print("abc");
-
-clear the lcd (this writes " " into all positions and is therefore slow):
-If only specific areas should be cleared use a mix of setCursor and print(" ") instead
+/*
+ * The logic used to control the LCD. See method body for more information.
+ * 
+ * This method completely blocks other logic-implementation for the time
+ * specified in {@link ex01-6#FLICKER_PREVENTION_TIME}.
+ * 
+ * @param mAnalogValue
+ *              An integer value which gets printed onto the LCDs screen.
+ */
+void lcdLogic(const int mAnalogValue) {
+  
+  // Clear display each time this method gets called, so the 
+  // old LCD-display doesn't get overwritten
   lcd.clear();
-*/
+
+  // Sets the cursor to the first row at the first char
+  lcd.setCursor(0,0);
+
+  // Prints the value given onto the LCD
+  lcd.print("Analog 0: " + String(mAnalogValue));
+
+  //Sets the cursor the second row at the first char
+  lcd.setCursor(0,1);
+
+  // Prints the value of the button which got pressed at the method call
+  lcd.print("button: " + buttonToString(getCurrentButton(mAnalogValue)));
+  delay(FLICKER_PREVENTION_TIME);
+}
+
+/**
+ * Converts the given integer value associated with a button to
+ * a String in a fashionable way.
+ * 
+ * @param mButtonValue
+ *              An integer value associated with a specific button.
+ */
+String buttonToString(const int mButtonValue) {
+
+  // no button was pressed
+  if(mButtonValue == 0){
+    return "-";
+
+  } else {
+    return "S"+String(mButtonValue);
+    
+  }
+}
+
+/*
+ * Gets the current integer value associated with the button wanted.
+ * Does so by extracting the button out of the analog value given.
+ * Those values aren't constant, thus thresholds are used to
+ * identify the appropriate button.
+ * 
+ * Because values are not static, we always use the middle between
+ * the value for this button and the next higher.
+ * Eg: Button1: 0, Button2: 241 ((241 + 0) / 2) + 0 = 121 
+ * 
+ * @param mAnalogValue
+ *              The value of the analog pin, used to identify the 
+ *              button pressed
+ * 
+ */
+int getCurrentButton(const int mAnalogValue) {
+  
+  if(mAnalogValue < FIRST_BUTTON_THRESHOLD) {
+    return 1;
+    
+  }else if(mAnalogValue < SECOND_BUTTON_THRESHOLD) {
+    return 2;
+    
+  }else if(mAnalogValue < THIRD_BUTTON_THRESHOLD) {
+    return 3;
+    
+  }else if(mAnalogValue < FOURTH_BUTTON_THRESHOLD) {
+    return 4;
+    
+  }else if(mAnalogValue < FIFTH_BUTTON_THRESHOLD) {
+    return 5;
+
+  // no button pressed
+  }else {
+    return 0;
+    
+  }
+}
