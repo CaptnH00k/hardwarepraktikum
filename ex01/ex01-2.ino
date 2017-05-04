cd712dfaf80a6db4c15bd67b39be919641a6d5f8
vickiedickie <immernochnichtwach@hab-verschlafen.de>
2017-05-04 16:26:36 +0200

    added ex. 2 and 3
    leon was geht

---
+++ ex01/ex01-2.ino
@@ -0,0 +1,28 @@
+// LED - test : Blinking with 1Hz frequency
+// LEDs are connected to Pin 10 and 11
+
+// global variables
+int ledPin1 = 10;
+int ledPin2 = 11;
+
+// initialization
+void setup(){
+  // set ledPins as output
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+}
+
+
+void loop(){
+  // turn on LED 1 for 0.5 seconds and wait for 0.5 seconds
+  digitalWrite(ledPin1, HIGH);
+  delay(500);
+  digitalWrite(ledPin1, LOW);
+  delay(500);
+  
+  // turn on LED 2 for 0.5 seconds and wait for 0.5 seconds
+  digitalWrite(ledPin2, HIGH);
+  delay(500);
+  digitalWrite(ledPin2, LOW);
+  delay(500);
+}
---
+++ ex01/ex01-3.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(10, OUTPUT); // red
+  pinMode(11, OUTPUT); // yellow
+  pinMode(12, OUTPUT); // green
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // turn on the red LED for 5 seconds (don't turn it off)
+  digitalWrite(10, HIGH);   
+  delay(5000);            
+
+  // turn on the yellow LED (red is still on for 1 second) and turn both off after 1 second
+  digitalWrite(11, HIGH); 
+  delay(1000);            
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);  
+
+  // turn on the green LED for 3 seconds 
+  digitalWrite(12, HIGH);   
+  delay(3000);              
+  digitalWrite(12, LOW);    
+
+  // turn on the yellow LED for 1 second  
+  digitalWrite(11, HIGH);   
+  delay(1000);                       
+  digitalWrite(11, LOW);
+
+}
