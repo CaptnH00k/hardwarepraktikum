cdd88b5a3af7d746e5d6ffc0ebe7c59929243d1d
CaptnH00k <leon.gnaedinger@gmail.com>
2017-05-04 17:12:47 +0200

    now displaying analog value again and added comments

--- ex01/ex01-6.ino
+++ ex01/ex01-6.ino
@@ -11,14 +11,13 @@
 */
 
 // include LCD functions:
-#include <LiquidCrystal.h> 
+#include <LiquidCrystal.h>
 
 // define the LCD screen
 LiquidCrystal lcd(3, 4, 6, 7, 8, 9);
 
 // global variable, stores the result from analog pin
 int analogValue;
-int test = 5;
 
 void setup()
 {
@@ -29,12 +28,18 @@ void setup()
 
 void loop()
 {
-  lcd.clear();
-  lcd.print("button: " + whichButtonIsPressedAsString());
+  // Because values are not static, we always use the middle between
+  analogValue = analogRead(A0);
+  lcd.clear(); // Clear display to not overwrite stupid things
+  lcd.setCursor(0,0); // Set cursor to first line
+  lcd.print("Analog 0: " + String(analogValue)); // print out the analog value
+  lcd.setCursor(0,1); // set cursor to second line
+  lcd.print("button: " + whichButtonIsPressedAsString()); // print out which button is pressed
    // wait for 100 ms (reduces display flickering)
   delay(100);
 }
 
+// Returns which button is pressed as a string. "S1-5" => Button 1-5, "-"=> no button is pressed
 String whichButtonIsPressedAsString() {
   int buttonPressed = whichButtonIsPressed();
   if(buttonPressed == 0){
@@ -44,9 +49,12 @@ String whichButtonIsPressedAsString() {
   }
 }
 
+// Returns which button is pressed. 1-5 => Button 1-5, 0=> no button is pressed
 int whichButtonIsPressed()
 {
-  analogValue = analogRead(A0);
+  // Because values are not static, we always use the middle between
+  // the value for this button and the next higher.
+  // Eg: Button1: 0, Button2: 241 ((241 + 0) / 2) + 0 = 121 
   if(analogValue < 121) {
     return 1;
   }else if(analogValue < 357) {
@@ -58,14 +66,15 @@ int whichButtonIsPressed()
   }else if(analogValue < 927) {
     return 5;
   }else {
+    // no button pressed
     return 0;
-  }  
+  }
 }
 
 
 /* Usefull LCD functions:
 set the current write position of the lcd to specific line and row:
-  lcd.setCursor(row, line);  
+  lcd.setCursor(row, line);
 
 write onto lcd, starting at current position:
   lcd.print("abc");
