86597df37c1f50add59ee3ea20c02811e8c8a2fb
Ativelox <ativelox.dev@web.de>
2017-05-27 21:55:41 +0200

    Added documentation and .zip file for the final release.
    Signed-off-by: Ativelox <ativelox.dev@web.de>

--- ex03/ex03-1_3/ex03-1_3.ino
+++ ex03/ex03-1_3/ex03-1_3.ino
@@ -1,4 +1,8 @@
-/*******************
+/*
+ * This class provides low-level methods to set bits in specified registers
+ * to send HIGH or LOW values to Pin12. Also provides time measurements,
+ * comparing whether C, or Assembler works faster.
+ * 
  * Testergebnisse:
  * SERIAL OUTPUT:
  * > TEST C, LETS A GO!
@@ -12,12 +16,21 @@
  * Komischerweise ist unser C code schneller als unser ASM code.
  * Unerklärbar?!
  */
-
+ 
+/*
+ * This method gets called once, before the update cycle starts ticking.
+ */
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);      // open the serial port at 9600 bps:
+  
 }
 
+/*
+ * The main-loop of this program. Gets repeatedly called without delay.
+ * 
+ * @see ex02-1_3#testC()
+ * @see ex02-1_3#testAsm()
+ */
 void loop() {
   // put your main code here, to run repeatedly:
   testC();
@@ -25,11 +38,15 @@ void loop() {
   delay(5000);
 }
 
+/**
+ * Time measurement for testing the C-Code used to set Pin12 to HIGH or Low.
+ */
 void testC() {
   Serial.println("TEST C, LETS A GO!");
   unsigned long start = micros();
   for(unsigned long i = 0;i < 100000;i++) {
-   //Serial.println(String(i)); //make sure we reach the 100.000! uncomment this if you want valid test results
+    //make sure we reach the 100.000! uncomment this if you want valid test results
+    //Serial.println(String(i)); 
     setPin12(true);
     setPin12(false);
   }
@@ -41,11 +58,15 @@ void testC() {
   
 }
 
+/**
+ * Time measurement for testing the Assembler-Code used to set Pin12 to HIGH or LOW.
+ */
 void testAsm() {
   Serial.println("TEST ASM, LETS A GO!");
   unsigned long start = micros();
   for(unsigned long i = 0;i < 100000;i++) {
-    //Serial.println(String(i)); //make sure we reach the 100.000! uncomment this if you want valid test results
+    //make sure we reach the 100.000! uncomment this if you want valid test results
+    //Serial.println(String(i)); 
     setPin12Asm(true);
     setPin12Asm(false);
   }
@@ -57,17 +78,34 @@ void testAsm() {
   
 }
 
-
-void setPin12(boolean high) {
-  if(high == true) {
+/**
+ * Sets pin 12 to the given boolean value, where true equals 
+ * HIGH and false equals LOW. Sets bits directly in the appropriate
+ * register to achieve the desired effect.
+ * 
+ * @param mHigh
+ *          The boolean determining whether the pin gets set
+ *          to HIGH or LOW.
+ */
+void setPin12(const boolean mHigh) {
+  if(mHigh == true) {
     PORTB |= (1 << 4);
   } else {
     PORTB &= ~(1 << 4);  
   }
 }
 
-void setPin12Asm(boolean high) {
-  if(high == true) {
+/**
+ * Sets pin 12 to the given boolean value, where true equals
+ * HIGH and false equals LOW. Does so by using assembler
+ * and setting bits directly in the appropriate register.
+ * 
+ * @param mHigh
+ *         The boolean determining whether the pin gets set
+ *         to HIGH or LOW
+ */
+void setPin12Asm(const boolean mHigh) {
+  if(mHigh == true) {
     // Set 4th bit to one in register PORTB 
     asm volatile (
     "sbi %0, %1\n\t" // 2 clocks
--- ex03/ex03-4_8/ex03-4_8.ino
+++ ex03/ex03-4_8/ex03-4_8.ino
@@ -1,23 +1,41 @@
+/*
+ * This class is able to set timer interruptions so it can play 
+ * sounds and toggle LEDs at random intervalls.
+ */
+
+//holding the value of the pin the speaker is using
 const uint8_t SPEAKER_PIN = 12;
 
+//holding whether the speaker pin is HIGH or LOW
 boolean on = false;
 
+//holding whether the LED is HIGH or LOW
 boolean ledIsOn = false;
 
+//holding 10 random values between 500 and 2500
 uint32_t duration[10];
 
+//holding 10 random value between 100 and 2000
 uint32_t frequency[10];
 
+//holding the value of the pin the LED is using
 const uint8_t LED_PIN = 11;
 
+//holding the value of how many milliseconds have passed since program start.
 volatile uint32_t tCount = 0;
 
+//a random value which is based off of tcount and a random number
 volatile uint32_t sCount = 0;
 
+//index which gets incremented when sCount > tCount
 volatile uint8_t index = 0;
 
+//the values the prescaler can have
 const uint32_t prescalers[6] = {8,32,64,128,256,1024};
 
+/*
+ * This method gets called once, before the update cycle starts ticking.
+ */
 void setup() {
   Serial.begin(38400);
 
@@ -33,13 +51,24 @@ void setup() {
   
 }
 
+/**
+ * Sets a timer interruption event for the second timer. Sends the given PIN
+ * in the given frequency HIGHS and LOWS.
+ * 
+ * @param mPin 
+ *          The pin on which to use PWM.
+ * @param mFrequency 
+ *          The frequency at which to send HIGH and LOW.
+ */
 void setTimerTwoInterruptionEvent(const uint8_t mPin, unsigned long mFrequency){
 
+  //don't do anything if the frequency isn't in the given bounds.
   if(mFrequency < 100 || mFrequency > 2000){
     return;
     
   }
-  
+
+  //calculate the TOP for the OCR2A
   unsigned long coreFrequencyHz = 8000000;
   unsigned long runsPerSecond = mFrequency * 2;
   unsigned long counterOverflowReset =  coreFrequencyHz / runsPerSecond;
@@ -67,6 +96,7 @@ void setTimerTwoInterruptionEvent(const uint8_t mPin, unsigned long mFrequency){
   TCCR2A = 0;
   TCCR2B = 0;  
 
+  //set the prescale so the value for the OCR2A is in the bounds (0...255)
   if(prescale == 1) {
     TCCR2B |= (1 << CS20); 
   } else if(prescale == 8) {
@@ -103,31 +133,46 @@ void setTimerTwoInterruptionEvent(const uint8_t mPin, unsigned long mFrequency){
   
 }
 
+/**
+ * Sets a timer interruption event for the first timer.
+ */
 void setTimerOneInterruptionEvent(){
 
+  //disable all interrupts.
   cli();
-  
+
+  //reset registers
   TCCR1A = 0;
   TCCR1B = 0;
-  TCNT1 = 0;
 
+  //set the OCR1A register to the correct value to count up once per millisecond.
   OCR1A = 7999;
 
+  // set mode CTC
   TCCR1B |= (1 << WGM12);
 
+  // set the prescale 
   TCCR1B |= (1 << CS10);
 
+  // enable interrupt
   TIMSK1 |= (1 << OCIE1A);
 
+  //enable all interrupts
   sei();
 }
 
+/*
+ * The main-loop of this program. Gets repeatedly called without delay.
+ */
 void loop(){
   Serial.println(String(tCount));
   delay(1000);
   
 }
 
+/**
+ * Toggle the given LED, if it's ON turn it OFF and vice versa.
+ */
 void toggleLED(const int mLED){
   if(ledIsOn){
     digitalWrite(mLED, LOW);
